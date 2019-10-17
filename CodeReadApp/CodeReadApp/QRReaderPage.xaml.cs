d0567b63d85cb78f62045f5a5b7d69e27b4e123c
Juan Santos <juansantos.pc@gmail.com>
2019-10-16 23:11:51 -0300

    app Xamarin

---
+++ CodeReadApp/CodeReadApp/QRReaderPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Common.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ZXing.Net.Mobile.Forms;
+
+namespace CodeReadApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class QRReaderPage : ContentPage
+    {
+        public QRReaderPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Scanner();
+        }
+
+        private async void Scanner()
+        {
+            var scannerPage = new ZXingScannerPage();
+
+            scannerPage.Title = "Lector de QR";
+            scannerPage.OnScanResult += (result) =>
+            {
+                scannerPage.IsScanning = false;
+
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    Navigation.PopAsync();
+                    AESController aes = new AESController();
+                    string textDecrypt = aes.Decrypt(result.Text);
+
+                    DisplayAlert("Valor Obtenido", textDecrypt, "OK");
+                });
+            };
+
+            await Navigation.PushAsync(scannerPage);
+        }
+    }
+}
